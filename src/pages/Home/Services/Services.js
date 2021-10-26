11d0acbdf0e94b4e4c84e113746bef6d13f7997f
learnwithkawsar <kawsar721g@gmail.com>
2021-10-27 01:43:06 +0600

    use API

--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -2,18 +2,20 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Container, Row } from "react-bootstrap";
+import Url from "../../../utilits/url";
 import Service from "../Service/Service";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const url = Url("services");
+  console.log(url);
   useEffect(() => {
-    fetch("./services.json")
+    fetch(url)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [url]);
   return (
     <>
-    
       <Container className="pt-5" id="services">
         <Row className="g-3">
           <>
---
+++ src/utilits/url.js
@@ -0,0 +1,4 @@
+const Url = (url) => {
+  return `https://genius-car-mechanics-server.herokuapp.com/${url}`;
+};
+export default Url;
